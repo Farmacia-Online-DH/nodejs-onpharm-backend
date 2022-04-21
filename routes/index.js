40d9ea8e2ed005f833f1cf8d873e2f51f8818ba2
Lucas <lucas.av.dias1991@gmail.com>
2022-04-21 15:07:13 -0300

    carrinho

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,57 @@
 module.exports = (app) => {
+  //--------------------Usuarios
 
-    //--------------------Usuarios
+  app.get('/usuarios', app.api.usuario.getUsuarios);
+  app.post('/usuarios', app.api.usuario.postUsuario);
+  app.put('/usuarios/:id', app.api.usuario.updateUsuario);
+  app.delete('/usuarios/:id', app.api.usuario.deleteUsuario);
 
-    app.get('/usuarios', app.api.usuario.getUsuarios)
-    app.post('/usuarios', app.api.usuario.postUsuario)
-    app.put('/usuarios/:id', app.api.usuario.updateUsuario)
-    app.delete('/usuarios/:id', app.api.usuario.deleteUsuario)
+  //--------------------Farmacias (Rafa)
 
-    //--------------------Farmacias (Rafa)
+  app.get('/farmacias', app.api.farmacia.getFarmacias);
+  app.post('/farmacias', app.api.farmacia.postFarmacia);
+  app.put('/farmacias/:id', app.api.farmacia.updateFarmacia);
+  app.delete('/farmacias/:id', app.api.farmacia.deleteFarmacia);
 
-    app.get('/farmacias', app.api.farmacia.getFarmacias)
-    app.post('/farmacias', app.api.farmacia.postFarmacia)
-    app.put('/farmacias/:id', app.api.farmacia.updateFarmacia)
-    app.delete('/farmacias/:id', app.api.farmacia.deleteFarmacia)
+  //--------------------Endereços User (Guilherme)
 
-    //--------------------Endereços User (Guilherme)
+  app.get('/enderecos-user', app.api.enderecoUser.getEnderecosUser);
+  app.post('/enderecos-user', app.api.enderecoUser.postEnderecosUser);
+  app.put('/enderecos-user/:id', app.api.enderecoUser.updateEnderecosUser);
+  app.delete('/enderecos-user/:id', app.api.enderecoUser.deleteEnderecosUser);
 
-    app.get('/enderecos-user', app.api.enderecoUser.getEnderecosUser)
-    app.post('/enderecos-user', app.api.enderecoUser.postEnderecosUser)
-    app.put('/enderecos-user/:id', app.api.enderecoUser.updateEnderecosUser)
-    app.delete('/enderecos-user/:id', app.api.enderecoUser.deleteEnderecosUser)
+  //---------Endereços Farmácias (Rafa)
 
+  app.get('/enderecoFarma', app.api.enderecoFarma.getEnderecoFarmas);
+  app.post('/enderecoFarma', app.api.enderecoFarma.postEnderecoFarma);
+  app.put('/enderecoFarma/:id', app.api.enderecoFarma.updateEnderecoFarma);
+  app.delete('/enderecoFarma/:id', app.api.enderecoFarma.deleteEnderecoFarma);
 
-    //---------Endereços Farmácias (Rafa)
+  //---------FormaPgto - User (Rafa)
 
-    app.get('/enderecoFarma', app.api.enderecoFarma.getEnderecoFarmas)
-    app.post('/enderecoFarma', app.api.enderecoFarma.postEnderecoFarma)
-    app.put('/enderecoFarma/:id', app.api.enderecoFarma.updateEnderecoFarma)
-    app.delete('/enderecoFarma/:id', app.api.enderecoFarma.deleteEnderecoFarma)
+  app.get('/formaPgto', app.api.formaPgto.getFormaPgtos);
+  app.post('/formaPgto', app.api.formaPgto.postFormaPgto);
+  app.put('/formaPgto/:id', app.api.formaPgto.updateFormaPgto);
+  app.delete('/formaPgto/:id', app.api.formaPgto.deleteFormaPgto);
 
-    //---------FormaPgto - User (Rafa)
+  //---------Pedido (Rafa)
 
-    app.get('/formaPgto', app.api.formaPgto.getFormaPgtos)
-    app.post('/formaPgto', app.api.formaPgto.postFormaPgto)
-    app.put('/formaPgto/:id', app.api.formaPgto.updateFormaPgto)
-    app.delete('/formaPgto/:id', app.api.formaPgto.deleteFormaPgto)
+  app.get('/pedidos', app.api.pedido.getPedidos);
+  app.post('/pedidos', app.api.pedido.postPedido);
+  app.put('/pedidos/:id', app.api.pedido.updatePedido);
+  app.delete('/pedidos/:id', app.api.pedido.deletePedido);
 
-    //---------Pedido (Rafa)
+  //----Produtos (Guilherme)
 
-    app.get('/pedidos', app.api.pedido.getPedidos)
-    app.post('/pedidos', app.api.pedido.postPedido)
-    app.put('/pedidos/:id', app.api.pedido.updatePedido)
-    app.delete('/pedidos/:id', app.api.pedido.deletePedido)
+  app.get('/produtos', app.api.produto.getProdutos);
+  app.post('/produtos', app.api.produto.postProdutos);
+  app.put('/produtos/:id', app.api.produto.updateProdutos);
+  app.delete('/produtos/:id', app.api.produto.deleteProdutos);
 
-    //----Produtos (Guilherme)
+  //----Carrinho
 
-    app.get('/produtos', app.api.produto.getProdutos)
-    app.post('/produtos', app.api.produto.postProdutos)
-    app.put('/produtos/:id', app.api.produto.updateProdutos)
-    app.delete('/produtos/:id', app.api.produto.deleteProdutos)
-    
-    //----Carrinho
-
-    app.get('/carrinhos', app.api.carrinho.getCarrinhos)
-
-}
+  app.get('/carrinhos', app.api.carrinho.getCarrinhos);
+  app.post('/carrinhos', app.api.carrinho.postCarrinhos);
+  app.put('/carrinhos/:id', app.api.carrinho.putCarrinhos);
+  app.delete('/carrinhos/:id', app.api.carrinho.deleteCarrinhos);
+};
